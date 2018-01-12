b6996c9593477d5965f38528e1024464d1dbc007
stav alfi <stavalfi@gmail.com>
2018-01-12 15:21:22 +0200

    fixed bug in registration

--- app/src/main/java/com/example/stavalfi/app1/RegistrationActivity.java
+++ app/src/main/java/com/example/stavalfi/app1/RegistrationActivity.java
@@ -3,13 +3,12 @@ package com.example.stavalfi.app1;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -119,6 +118,7 @@ public class RegistrationActivity extends AppCompatActivity {
         if (resultCode != RESULT_OK) {
             // something went deeply wrong.
             Toast.makeText(getApplicationContext(), getApplicationContext().getResources().getString(R.string.error), Toast.LENGTH_SHORT).show();
+            return;
         }
 
         switch (requestCode) {
